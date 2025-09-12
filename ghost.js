67998845158dd268be16c2a7b20ec3712c640483
Kava <154646695+KerhylonKava@users.noreply.github.com>
2025-09-12 14:52:04 -0700

    Basic bug fixes around ghost name, moved ghost to secondary page, main page has project links

--- ghost.js
+++ ghost.js
@@ -4,9 +4,17 @@ function updateName() {
     var displayElement = document.getElementById("displayName"); // Find the element to display the ghost's name
     
     // Check if the user entered something
-    if (ghostName.trim() !== "") {
+    if (ghostName.trim() == "Eddy") {
         displayElement.textContent = "Hello, I'm " + ghostName + "!"; // ghost talking
-    } else {
+        //add something about the weather
+    } 
+    else if (ghostName.trim() == "Esther") {
+        displayElement.textContent = "Oh, that's actually the name of the programmer for this website... A different name maybe?";
+    }
+    else if (ghostName.trim() !== "") {
+        displayElement.textContent = "Hello, my name is " + ghostName + "!";
+    }
+    else if (ghostName.trim() == "") {
         displayElement.textContent = "I guess I'm nameless :("; // Default message if no input (sad ghost)
     }
 }
