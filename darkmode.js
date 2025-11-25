5962278e3d2c6fa33e8cb784b612e7b042bfe1eb
Kava <154646695+KerhylonKava@users.noreply.github.com>
2025-11-24 20:27:54 -0800

    Darkmode/Lightmode toggle working!

---
+++ darkmode.js
@@ -0,0 +1,30 @@
+let darkmode = localStorage.getItem("darkmode")
+const themeToggle = document.getElementById("themeToggle")
+
+const enableDarkMode = () => {
+    document.body.classList.add("darkmode")
+    localStorage.setItem("darkmode", "active")
+
+}
+
+const disableDarkMode = () => {
+    document.body.classList.remove("darkmode")
+    localStorage.setItem("darkmode", null)
+}
+
+if (darkmode === "active") enableDarkMode()
+
+themeToggle.addEventListener("click", () =>{
+    darkmode = localStorage.getItem("darkmode")
+    darkmode !== "active" ? enableDarkMode() : disableDarkMode()
+})
+
+/* darkmode !== "active" ? enableDarkMode() : disableDarkMode() same as:*/
+/*
+if (darkmode !== "active"){
+    enableDarkMode()
+}
+else{
+    disableDarkMode()
+}
+*/
